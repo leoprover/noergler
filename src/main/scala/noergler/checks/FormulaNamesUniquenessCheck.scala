97efca8f6d96dbcdb7a7bcfbc5675d15ffff83bd
Alexander Steen <alexander.steen@uni-greifswald.de>
2026-03-27 14:33:22 +0100

    naive name uniqueness check

---
+++ src/main/scala/noergler/checks/FormulaNamesUniquenessCheck.scala
@@ -0,0 +1,23 @@
+package noergler.checks
+
+import leo.datastructures.TPTP
+
+class FormulaNamesUniquenessCheck {
+
+}
+
+object FormulaNamesUniquenessCheck {
+  import scala.collection.mutable
+  final def apply(proofSteps: Seq[TPTP.FOFAnnotated]): Boolean = {
+    var soFarSoGood = true
+    val usedNames: mutable.Set[String] = mutable.Set.empty
+    val proofStepsIt = proofSteps.iterator
+    while (proofStepsIt.hasNext && soFarSoGood) {
+      val proofStep = proofStepsIt.next()
+      val name = proofStep.name
+      if (usedNames.contains(name)) soFarSoGood = false
+      usedNames.addOne(name)
+    }
+    soFarSoGood
+  }
+}
