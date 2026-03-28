94b7b40564a322ac4bc3e493ca712d22e1db5637
Alexander Steen <alexander.steen@uni-greifswald.de>
2026-03-28 13:08:05 +0100

    add try-catch-structure to controller for collecting failed verification checks

--- src/main/scala/noergler/ProofCheckController.scala
+++ src/main/scala/noergler/ProofCheckController.scala
@@ -1,7 +1,7 @@
 package noergler
 
 import leo.datastructures.TPTP
-import noergler.ProofCheckController.Configuration
+import noergler.ProofCheckController.{Configuration, VerificationFailedException}
 import noergler.checks.{ConjectureNegationCheck, CorrectFormulaFromFileCheck, FormulaNamesUniquenessCheck, GenericTHMInferenceCheck, InferenceParentsAcyclicityCheck, InferenceParentsExistCheck, ProofEndsInFalseCheck, SkolemizationCheck}
 
 import java.util.concurrent.Executors
@@ -70,148 +70,155 @@ class ProofCheckController(problem: TPTP.Problem,
     //////////////////////////////////////////////////////////
     // create/start check tasks. for first iteration maybe just sequential?
     logger.fine("Processing proof ...")
-    /* TODO: The results of the proof checking are not yet collected.
-       But since it is planned to use Futures or similar to execute, maybe
-       we just skip a principled approach here and manually check for now */
-    //////////////////
-    // specific checks
-    //////////////////
-    // (I.1) does the proof end with false?
-    checkProofEndsInFalse()
-    // (I.2) are the annotated formulas names in the proof unique?
-    checkFormulaNamesAreUnique()
-    // (I.3) are the inference parents acyclic?
-    checkInferencesAreAcyclic()
-    // ... more?
-
-    //////////////////
-    // iteration over every proof line, check each line depending on its character:
-    //////////////////
-    for (proofstep <- proofSteps) {
-      logger.finer(s"Checking proof step '${proofstep.name}' with annotation '${proofstep.annotations.map(_._1.pretty).getOrElse("")}' ...")
-      //////
-      // II. Checks that every line has to do regardless of their specific annotation
-      //////
-      // (II.1) check that every inference parent (if any) actually exists (earlier in the proof)
-      checkInferenceParentsExist(proofstep)
-      // (II.2) check that role of proof step is admissible
-      checkRole(proofstep)
+    try {
+      //////////////////
+      // specific checks
+      //////////////////
+      // (I.1) does the proof end with false?
+      checkProofEndsInFalse()
+      // (I.2) are the annotated formulas names in the proof unique?
+      checkFormulaNamesAreUnique()
+      // (I.3) are the inference parents acyclic?
+      checkInferencesAreAcyclic()
       // ... more?
 
-      //////
-      // III. Checks that every line has to do individually, specific to their annotation
-      //////
-      val annotation = proofstep.annotations
-      annotationType(annotation) match {
-        case Some(annotationType) => annotationType match {
-          case "inference" =>
-            inferenceName(annotation) match {
-              case Some(inference) => inference match {
-                // (III.1) if a "negated_conjecture" entry, does it correctly negate and has correct role?
-                case "negated_conjecture" => checkNegatedInference(proofstep)
-                // (III.2) if a "skolemize" entry, does it correctly skolemize (use ASK)
-                case "skolemize" => checkSkolemization(proofstep)
-                // (III.3) if generic status(thm) entry, does it follow from its parents? (using external ATPs)
-                case rule if inferenceStatus(annotation).getOrElse("") == "thm" => checkTHMInference(rule, proofstep)
-                case _ => // Error case: unknown inference rule with non-THM status
-                  logger.severe(s"Unknown inference '$inference' with non-thm status in proof step '${proofstep.name}'.")
-                // TODO: Handle error someshow, failed verified/not verified?
+      //////////////////
+      // iteration over every proof line, check each line depending on its character:
+      //////////////////
+      for (proofstep <- proofSteps) {
+        logger.finer(s"Checking proof step '${proofstep.name}' with annotation '${proofstep.annotations.map(_._1.pretty).getOrElse("")}' ...")
+        //////
+        // II. Checks that every line has to do regardless of their specific annotation
+        //////
+        // (II.1) check that every inference parent (if any) actually exists (earlier in the proof)
+        checkInferenceParentsExist(proofstep)
+        // (II.2) check that role of proof step is admissible
+        checkRole(proofstep)
+        // ... more?
+
+        //////
+        // III. Checks that every line has to do individually, specific to their annotation
+        //////
+        val annotation = proofstep.annotations
+        annotationType(annotation) match {
+          case Some(annotationType) => annotationType match {
+            case "inference" =>
+              inferenceName(annotation) match {
+                case Some(inference) => inference match {
+                  // (III.1) if a "negated_conjecture" entry, does it correctly negate and has correct role?
+                  case "negated_conjecture" => checkNegatedInference(proofstep)
+                  // (III.2) if a "skolemize" entry, does it correctly skolemize (use ASK)
+                  case "skolemize" => checkSkolemization(proofstep)
+                  // (III.3) if generic status(thm) entry, does it follow from its parents? (using external ATPs)
+                  case rule if inferenceStatus(annotation).getOrElse("") == "thm" => checkTHMInference(rule, proofstep)
+                  case _ => // Error case: unknown inference rule with non-THM status
+                    logger.severe(s"Unknown inference '$inference' with non-thm status in proof step '${proofstep.name}'.")
+                    throw new VerificationFailedException(s"Proof step '${proofstep.name}' uses unknown inference rule '$inference' with non-thm status, cannot be checked.")
+                }
+                case None => // Unknown annotation, abort
+                  logger.severe(s"Annotation of proof step '${proofstep.name}' unknown.")
+                  throw new VerificationFailedException(s"Proof step '${proofstep.name}' uses malformed inference annotation.")
               }
-              case None => // Unknown annotation, abort
-                logger.severe(s"Annotation of proof step '${proofstep.name}' unknown.")
-              // TODO: Handle error someshow, failed verified
-            }
-          case "file" => checkFormulaFromFile(proofstep)
-          case _ => ???
+            case "file" => checkFormulaFromFile(proofstep)
+            case "introduced" => ??? // TODO
+            case record => throw new VerificationFailedException(s"Proof step '${proofstep.name}' uses unknown record '$record'.")
+          }
+          case None => // no annotation is an error for all steps.
+            logger.severe(s"Proof step '${proofstep.name}' has no or malformed annotation.")
+            throw new VerificationFailedException(s"Proof step '${proofstep.name}' has no or malformed annotation.")
         }
-        case None => // no annotation is an error for all steps.
-          logger.severe(s"Proof step '${proofstep.name}' has no or malformed annotation.")
-          // TODO: Handle somehow
+        logger.info(s"Check succeeded for step '${proofstep.name}'.") // Assuming we fail fast if anything happens before
       }
-      logger.info(s"Check succeeded for step '${proofstep.name}'.") // Assuming we fail fast if anything happens before
+
+      // TODO from here
+      // wait on completion of individual tasks
+      // how? probably just in the order above, as they must finish all anyway for
+      // it to be a success
+      // if some on the way fail -> fail
+      // if some on the way timeout -> ???
+      // if none fails -> success
+
+      // report success
+      Verified
+    } catch {
+      case e: VerificationFailedException =>
+        FailedVerified(e.getMessage)
     }
-
-    // TODO from here
-    // wait on completion of individual tasks
-    // how? probably just in the order above, as they must finish all anyway for
-    // it to be a success
-    // if some on the way fail -> fail
-    // if some on the way timeout -> ???
-    // if none fails -> success
-
-    // report success
-    Verified
-
-    NotVerified("gave up") // for now
   }
 
-  private def checkProofEndsInFalse(): Boolean = {
+  private def checkProofEndsInFalse(): Unit = {
     logger.fine("Check for $false at the end of proof.")
     val endsWithFalseCheck = ProofEndsInFalseCheck.apply(proof)
-    // TODO: Handle negative result somehow, failed verified
     logger.info(s"Proof ends in $$false: $endsWithFalseCheck")
-    endsWithFalseCheck
+    if (!endsWithFalseCheck) throw new VerificationFailedException("Proof does not end it false")
   }
 
-  private def checkFormulaNamesAreUnique(): Boolean = {
+  private def checkFormulaNamesAreUnique(): Unit = {
     logger.fine("Check for uniqueness of formula names.")
     val namesUniqueCheck = FormulaNamesUniquenessCheck.apply(proofSteps)
-    // TODO: Handle negative result somehow, failed verified
     logger.info(s"Formula names unique: $namesUniqueCheck")
-    namesUniqueCheck
+    if (!namesUniqueCheck) throw new VerificationFailedException("Names of annotated formulas in proof are not unique.")
   }
 
-  private def checkInferencesAreAcyclic(): Boolean = {
+  private def checkInferencesAreAcyclic(): Unit = {
     logger.fine("Check for acyclicity of inference parents.")
     val inferenceParentsAreAcyclic = InferenceParentsAcyclicityCheck.apply(proofSteps, proofFormulas)
     logger.info(s"Inference parents are acyclic: $inferenceParentsAreAcyclic")
-    inferenceParentsAreAcyclic
+    if (!inferenceParentsAreAcyclic) throw new VerificationFailedException("Graph of inference parents from $false contains a cycle.")
   }
 
-  private def checkInferenceParentsExist(proofstep: TPTP.FOFAnnotated): Boolean = {
+  private def checkInferenceParentsExist(proofstep: TPTP.FOFAnnotated): Unit = {
     logger.finer("Check for existence of inference parents (if any).")
     val inferenceParentsCheck = InferenceParentsExistCheck.apply(proofstep, proofSteps)
-    // TODO: Handle negative result somehow, failed verified
     logger.fine(s"Inference parents exist (${proofstep.name}): $inferenceParentsCheck")
-    inferenceParentsCheck
+    if (!inferenceParentsCheck) throw new VerificationFailedException(s"Proof step '${proofstep.name}' has unknown inference parents.")
   }
 
-  private def checkRole(proofstep: TPTP.FOFAnnotated): Boolean = {
+  private def checkRole(proofstep: TPTP.FOFAnnotated): Unit = {
     val allowedRoles = Seq("axiom", "conjecture", "negated_conjecture", "plain")
-    allowedRoles.contains(proofstep.role)
+    val roleCheck = allowedRoles.contains(proofstep.role)
+    if (!roleCheck) throw new VerificationFailedException(s"Proof step '${proofstep.name}' has unknown role.")
   }
 
-  private def checkNegatedInference(proofstep: TPTP.FOFAnnotated): Boolean = {
+  private def checkNegatedInference(proofstep: TPTP.FOFAnnotated): Unit = {
     logger.finer("Check for correct negation of conjecture")
     val checkNegation = ConjectureNegationCheck.apply(proofstep, problemConjectureName.flatMap(problemFormulas.get))
     logger.fine(s"Negation of conjecture correct (${proofstep.name}): $checkNegation")
-    checkNegation
+    if (!checkNegation) throw new VerificationFailedException("Negation of conjecture is incorrect.")
   }
 
-  private def checkSkolemization(proofstep: TPTP.FOFAnnotated): Boolean = {
+  private def checkSkolemization(proofstep: TPTP.FOFAnnotated): Unit = {
     logger.finer("Check for correct skolemization")
     val checkSkolemize = SkolemizationCheck.apply(proofstep, ???) // TODO
     logger.fine(s"Skolemization correct (${proofstep.name}): $checkSkolemize")
-    checkSkolemize
+    if (!checkSkolemize) throw new VerificationFailedException(s"Skolemization in step '${proofstep.name}' is incorrect.")
   }
 
-  private def checkTHMInference(rule: String, proofstep: TPTP.FOFAnnotated): Boolean = {
+  private def checkTHMInference(rule: String, proofstep: TPTP.FOFAnnotated): Unit = {
     logger.finer(s"Check for correct entailment of inference rule '$rule'.")
     val checkEntailment = GenericTHMInferenceCheck.apply(proofstep, proofFormulas)
+    // TODO: how to handle timeouts? Maybe Option[Boolean] instead of Boolean and None is for timeout?
+    //  So we can reschedule or so?
     logger.fine(s"Entailment correct (${proofstep.name}): $checkEntailment")
-    checkEntailment
+    if (!checkEntailment) throw new VerificationFailedException(s"Proof step '${proofstep.name}' cannot be verified.")
   }
 
-  private def checkFormulaFromFile(proofstep: TPTP.FOFAnnotated): Boolean = {
+  private def checkFormulaFromFile(proofstep: TPTP.FOFAnnotated): Unit = {
     logger.finer("Check for correct premise usage from problem file.")
-    val checkPremise = CorrectFormulaFromFileCheck.apply(proofstep, problemFormulas)
-    logger.fine(s"Formula equivalent to problem statement (${proofstep.name}): $checkPremise")
-    checkPremise
+    val checkFormulaFromFile = CorrectFormulaFromFileCheck.apply(proofstep, problemFormulas)
+    logger.fine(s"Formula equivalent to problem statement (${proofstep.name}): $checkFormulaFromFile")
+    if (!checkFormulaFromFile) throw new VerificationFailedException(s"Proof step '${proofstep.name}' does not use correct formula from file.")
   }
 }
 object ProofCheckController {
   final case class Configuration(timeout: Int,
                                  coreCount: Int)
+
+  /** Thrown during the check if some step yields that the proof definitely cannot be verified
+   * because it's not a valid proof. */
+  private class VerificationFailedException(msg: String) extends RuntimeException(msg)
+  /** Thrown during the check by some step if it gives up (e.g. timeout of external prover). */
+  private class NotVerifiedException(msg: String) extends RuntimeException(msg)
 
   private final val defaultTimeout: Int = 60
   private final val defaultCoreCount: Int = 1
