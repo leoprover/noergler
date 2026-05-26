63181233615565ffef5dc2ad9f1c8422be18d6e4
Alexander Steen <alexander.steen@uni-greifswald.de>
2026-05-26 12:26:56 +0200

    refactor name check

--- src/main/scala/noergler/checks/CorrectFormulaFromFileCheck.scala
+++ src/main/scala/noergler/checks/CorrectFormulaFromFileCheck.scala
@@ -36,30 +36,23 @@ object CorrectFormulaFromFileCheck {
             try {
               val fullPathReconstructedFromFormulaAnnotation = proofPath.getParent.resolve(stripQuotes(file)).toAbsolutePath.normalize()
               logger.finer(s"fullPathReconstructedFromFormulaAnnotation = ${fullPathReconstructedFromFormulaAnnotation.toString}")
-              if (problemPath.normalize() == fullPathReconstructedFromFormulaAnnotation) {
-                if (formulaFromProblem.role == proofstep.role) {
-                  if (formulaFromProblem.formula == proofstep.formula) {
-                    None
-                  } else {
-                    @inline val msg = s"Formula in proof step ${proofstep.name} is not identical to formula $formulaName from problem file."
-                    logger.finer(msg)
-                    Some(msg)
-                  }
-                } else {
-                  @inline val msg = s"Role of proof step ${proofstep.name} (${proofstep.role}) do not match role of formula $formulaName from problem file (${formulaFromProblem.role})."
-                  logger.info(msg)
-                  if (relaxProblemCheck) {
-                    logger.info("Continuing check, because option --relax-annotation-format is set.")
-                    None
-                  } else {
-                    Some(msg)
-                  }
-                }
-              } else {
+              if (problemPath.normalize() != fullPathReconstructedFromFormulaAnnotation) {
                 @inline val msg = s"Problem file path does not match the file annotation in proof step '${proofstep.name}'."
                 logger.finer(msg)
-                Some(msg)
+                return Some(msg)
               }
+              if (formulaFromProblem.role != proofstep.role) {
+                @inline val msg = s"Role of proof step ${proofstep.name} (${proofstep.role}) do not match role of formula $formulaName from problem file (${formulaFromProblem.role})."
+                logger.info(msg)
+                if (relaxProblemCheck) logger.info("Continuing check, because option --relax-annotation-format is set.")
+                else return Some(msg)
+              }
+              if (formulaFromProblem.formula != proofstep.formula) {
+                @inline val msg = s"Formula in proof step ${proofstep.name} is not identical to formula $formulaName from problem file."
+                logger.finer(msg)
+                return Some(msg)
+              }
+              None
             } catch {
               case e:InvalidPathException =>
                 @inline val msg = s"File annotation path of proof step ${proofstep.name} malformed: ${e.toString}"
