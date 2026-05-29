ab1cb8dc7f7f9a653a06c69cfe6adbcd35fe48bc
Alexander Steen <alexander.steen@uni-greifswald.de>
2026-05-29 10:45:47 +0200

    update proover run script

--- contrib/proover_run.sh
+++ contrib/proover_run.sh
@@ -4,22 +4,23 @@
 
 # Resolve the full path of the script
 HERE=$(dirname "$(readlink -f "$0")")
-# Resolve the full file path of noergler
-NOERGLER=$(readlink -f "$1")
+# Resolve the full file path of noergler // TODO: Adapt to proper path when needed, e.g., relative to $HERE
+NOERGLER=$(which "noergler")
+# Resolve the full file path of eprover // TODO: Adapt to proper path when needed, e.g., relative to $HERE
+EPROVER=$(which "eprover")
+# Resolve the full file path of mace4 // TODO: Adapt to proper path when needed, e.g., relative to $HERE
+MACE4=$(which "mace4")
 # Resolve the full file path of the proof file
-PROOF=$(readlink -f "$2")
+PROOF=$(readlink -f "$1")
 # Path of proof file
 PROOFPATH=$(dirname "$PROOF")
 
-#echo "HERE: $HERE"
-#echo "PROOF: $PROOF"
-#echo "PROOFPATH: $PROOFPATH"
-#echo "NOERGLER: $NOERGLER"
 # Get "Problem: " annotation from proof file
 PROBLEMREL=$(cat "$PROOF" | grep "% Proof" | cut -d ":" -f 2 | tr -d ' ')
-#echo "RELATIVE PATH OF PROBLEM: $PROBLEMREL"
 PROBLEM="$PROOFPATH/$PROBLEMREL"
-#echo "ABSOLUTE PATH OF PROBLEM: $PROBLEM"
 
 ## Run
-$NOERGLER --problem "$PROBLEM" "$PROOF"
+#echo "% proof path: $PROOFPATH"
+#echo "% problem path: $PROBLEM"
+
+$NOERGLER --problem "$PROBLEM" --verbosity 1 --mace4-path "$MACE4" --eprover-path "$EPROVER" --parallel-mode steps --parallel-model-finder-mode offset "$PROOF"
