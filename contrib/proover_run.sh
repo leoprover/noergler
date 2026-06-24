8500b3e403a99d306f195ba139ce9f9c90f6e25f
Alexander Steen <alexander.steen@uni-greifswald.de>
2026-06-24 11:12:14 +0200

    add timeout to proover run script

--- contrib/proover_run.sh
+++ contrib/proover_run.sh
@@ -2,6 +2,11 @@
 ### Simple script for executing Nörgler in the ProoVer competition
 ### Alexander Steen, 2026
 
+if [[ $# -eq 0 ]] ; then
+    echo 'usage: proover_run.sh <proof path> <timeout>'
+    exit 0
+fi
+
 # Resolve the full path of the script
 HERE=$(dirname "$(readlink -f "$0")")
 # Resolve the full file path of noergler // TODO: Adapt to proper path when needed, e.g., relative to $HERE
@@ -14,13 +19,20 @@ MACE4=$(which "mace4")
 PROOF=$(readlink -f "$1")
 # Path of proof file
 PROOFPATH=$(dirname "$PROOF")
+# Timeout
+TIMEOUT="$2"
 
 # Get "Problem: " annotation from proof file
 PROBLEMREL=$(cat "$PROOF" | grep "% Proof" | cut -d ":" -f 2 | tr -d ' ')
 PROBLEM="$PROOFPATH/$PROBLEMREL"
 
 ## Run
-#echo "% proof path: $PROOFPATH"
-#echo "% problem path: $PROBLEM"
+RESULT=$(timeout --kill-after=0.5s "$TIMEOUT" $NOERGLER --problem "$PROBLEM" --verbosity 1 --mace4-path "$MACE4" --eprover-path "$EPROVER" --parallel-mode steps --parallel-model-finder-mode offset --find-failing-step "$PROOF") 
+EXITCODE=$?
 
-$NOERGLER --problem "$PROBLEM" --verbosity 1 --mace4-path "$MACE4" --eprover-path "$EPROVER" --parallel-mode steps --parallel-model-finder-mode offset --find-failing-step "$PROOF"
+if [[ $EXITCODE -eq 124 ]]; then
+    echo "% SZS status Timeout"
+else
+    echo "$RESULT"
+fi
+
